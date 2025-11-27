c381b6570c1a2bb088772e512836e8c8ffbea576
adtsyy <tasyadwia005@gmail.com>
2025-11-27 19:53:05 +0700

    tasya

--- my_sistem_akademik/my_sistem_akademik/urls.py
+++ my_sistem_akademik/my_sistem_akademik/urls.py
@@ -1,8 +1,18 @@
+# my_sistem_akademik/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from my_sistem_akademik import views  # pastikan views login/logout ada di sini
 
 urlpatterns = [
+    # Admin default Django
     path('admin/', admin.site.urls),
-    path('', include('admin_app.urls')),
-    path('laporan/', include('laporan.urls')),
+
+    # Auth
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+
+    # Aplikasi
+    path('admin_app/', include('admin_app.urls')),  # dashboard/admin_app
+    path('guru/', include('guru.urls')),           # dashboard/guru
+    path('laporan/', include('laporan.urls')),     # halaman laporan
 ]
