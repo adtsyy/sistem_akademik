e1493489f50a302ff8861a93b97bfdcc07958894
julianputrad2707 <julianputrad2707@students.unnes.ac.id>
2025-12-11 13:15:55 +0700

    hubungin laporan siswa

--- my_sistem_akademik/siswa/views.py
+++ my_sistem_akademik/siswa/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .models import Siswa, SPP, Nilai
+from .models import Siswa, Nilai
 from .forms import SiswaForm
 
-# Import Jadwal dari admin_app
+from laporan.models import SPP
 from admin_app.models import Jadwal as AdminJadwal
 from guru.models import Nilai as GuruNilai
 from guru.models import Absen
@@ -26,7 +26,7 @@ def siswa_dashboard(request):
     # -------------------------------------------------------
     # DATA SPP
     # -------------------------------------------------------
-    spp_data = siswa.spp_siswa.all().order_by('bulan')  # FIX: related_name spp_siswa
+    spp_data = siswa.spp_siswa.all().order_by('bulan')
 
     spp_summary = {
         "total_tunggakan": 0,
@@ -36,8 +36,8 @@ def siswa_dashboard(request):
     }
 
     for spp in spp_data:
-        if spp.status != "lunas":
-            spp_summary["bulan_belum_bayar"].append(spp.bulan)
+        if spp.status != "Lunas":
+            spp_summary["bulan_belum_bayar"].append(spp.get_bulan_display())
             spp_summary["spp_belum"] += 1
         else:
             spp_summary["spp_lunas"] += 1
